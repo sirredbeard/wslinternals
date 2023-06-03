477e3fc1d477acd3ef0573eb57dc88309febe5f5
Hayden Barnes <hayden.barnes@hpe.com>
2023-06-03 09:38:34 -0400

    docker-desktop fixes

--- list-wsl.ps1
+++ list-wsl.ps1
@@ -1,47 +1,45 @@
 $defaultGuid = (Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss").DefaultDistribution
 
 $wslDistributions = Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Lxss" | ForEach-Object {
-    $distribution = @{
-        Name = ""
-        "Linux Distro" = ""
-        "Distro Version" = ""
-        systemd = ""
-        "Default User" = ""
-        State = ""
-        WSL = ""
-    }
-
+    $distribution = @{}
     $distribution["Name"] = $_.GetValue("DistributionName")
 
-    $osRelease = Invoke-Expression "wsl.exe -d $($distribution["Name"]) cat /etc/os-release"
-    if ($osRelease) {
-        $distribution["Linux Distro"] = ($osRelease | Where-Object { $_ -like "PRETTY_NAME=*" }).Split("=")[1].Replace('"', '')
-        $distribution["Distro Version"] = ($osRelease | Where-Object { $_ -like "VERSION=*" }).Split("=")[1].Replace('"', '')
-    }
+    if ($distribution["Name"] -eq "docker-desktop") { $distribution["Linux Distro"] = "Docker Desktop"; $distribution["State"] = "Installed" }
+    if ($distribution["Name"] -eq "docker-desktop-data") { $distribution["Linux Distro"] = "Docker Desktop Data"; $distribution["State"] = "Installed" }
 
-    $wslConf = Invoke-Expression "wsl.exe -d $($distribution["Name"]) cat /etc/wsl.conf"
-    if ($wslConf) {
-        $distribution["systemd"] = ($wslConf | Where-Object { $_ -like "systemd=true" }).Count -gt 0
-    }
+    if ($distribution["Name"] -ne "docker-desktop" -and $distribution["Name"] -ne "docker-desktop-data" -and $distribution["Name"] -ne "docker-desktop-runtime") {
+        $osRelease = Invoke-Expression "wsl.exe -d $($distribution["Name"]) cat /etc/os-release"
+        if ($osRelease) {
+            $distribution["Linux Distro"] = ($osRelease | Where-Object { $_ -like "PRETTY_NAME=*" }).Split("=")[1].Replace('"', '')
+            $distribution["Distro Version"] = ($osRelease | Where-Object { $_ -like "VERSION=*" }).Split("=")[1].Replace('"', '')
+        }
 
-    $distribution["DefaultUid"] = $_.GetValue("DefaultUid")
+        $wslConf = Invoke-Expression "wsl.exe -d $($distribution["Name"]) cat /etc/wsl.conf"
+        if ($wslConf -and $wslConf.Contains("systemd=true")) {
+            $distribution["systemd"] = "Enabled"
+        } else {
+            $distribution["systemd"] = "Disabled"
+        }
 
-    $username = Invoke-Command -ScriptBlock { wsl.exe -d $($distribution["Name"]) -- id -un -- $args[0] } -ArgumentList $distribution["DefaultUid"] -ErrorAction SilentlyContinue
-    if ($username) {
-        $distribution["Default User"] = $username
-    }
+        $distribution["DefaultUid"] = $_.GetValue("DefaultUid")
 
-    $distribution["State"] = $_.GetValue("State")
-    switch ($distribution["State"]) {
-        0x1 { $distribution["State"] = "Installed" }
-        0x3 { $distribution["State"] = "Installing" }
-        0x4 { $distribution["State"] = "Uninstalling" }
-    }
+        $username = Invoke-Command -ScriptBlock { wsl.exe -d $($distribution["Name"]) -- id -un -- $args[0] } -ArgumentList $distribution["DefaultUid"] -ErrorAction SilentlyContinue
+        if ($username) {
+            $distribution["Default User"] = $username
+        }
 
-    $distribution["WSL"] = $_.GetValue("Version")
+        $distribution["State"] = $_.GetValue("State")
+        switch ($distribution["State"]) {
+            0x1 { $distribution["State"] = "Installed" }
+            0x3 { $distribution["State"] = "Installing" }
+            0x4 { $distribution["State"] = "Uninstalling" }
+        }
 
-    if ($defaultGuid -eq $_.PSChildName) {
-        $distribution["Name"] += "*"
+        $distribution["WSL"] = $_.GetValue("Version")
+
+        if ($defaultGuid -eq $_.PSChildName) {
+            $distribution["Name"] += "*"
+        }
     }
 
     New-Object -TypeName PSObject -Property $distribution
