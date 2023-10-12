0aa7143a2e63bac7688442c15958d6e646171e5d
Hayden Barnes <hayden.barnes@hpe.com>
2023-10-11 20:10:56 -0400

    Added support for Windows Terminal Preview to sysdistowt

--- sysdistrowt.ps1
+++ sysdistrowt.ps1
@@ -1,7 +1,8 @@
 $wtPath = Get-ChildItem "$env:LOCALAPPDATA\Packages\" -Directory | Where-Object { $_.Name -like "Microsoft.WindowsTerminal_*" } | Select-Object -ExpandProperty FullName
 $wtpPath = Get-ChildItem "$env:LOCALAPPDATA\Packages\" -Directory | Where-Object { $_.Name -like "Microsoft.WindowsTerminalPreview_*" } | Select-Object -ExpandProperty FullName
+$wtcPath = Get-ChildItem "$env:LOCALAPPDATA\Packages\" -Directory | Where-Object { $_.Name -like "Microsoft.WindowsTerminalCanary_*" } | Select-Object -ExpandProperty FullName
 
-if (!$wtPath -and !$wtpPath) {
+if (!$wtPath -and !$wtpPath -and !$wtcPath) {
     Write-Host "Windows Terminal is not installed."
     exit
 }
@@ -38,12 +39,34 @@ if ($wtPath) {
 }
 
 if ($wtpPath) {
-    Write-Host "Windows Terminal Preview is not installed."
+    Write-Host "Windows Terminal Preview is installed."
     if (Test-Path "$wtpPath\LocalState\settings.json") {
         Write-Host "Windows Terminal Preview settings.json found."
     } else {
         Write-Host "Windows Terminal Preview settings.json not found, creating..."
         $wtpExePath = Get-ChildItem "$env:ProgramFiles\WindowsApps\Microsoft.WindowsTerminalPreview_1*" -Directory | Select-Object -ExpandProperty FullName
+        $mywtProcess = Get-Process -Name "WindowsTerminal"
+        $mywtPid = $mywtProcess.Id
+        Start-Process "$wtpExePath\wt.exe"
+        Start-Sleep -Seconds 1
+        Get-Process -Name "WindowsTerminal" | Where-Object { $_.Id -ne $mywtPid } | Stop-Process
+    }
+    $wtpSettings = Get-Content "$wtpPath\LocalState\settings.json" -Raw | ConvertFrom-Json
+    if ($wtpSettings.profiles.list | Where-Object { $_.Name -eq $wslProfile.Name }) {
+        $wtpSettings.profiles.list = $wtpSettings.profiles.list | Where-Object { $_.Name -ne $wslProfile.Name }
+    }
+    $wtpSettings.profiles.list += $wslProfile
+    $wtpSettings | ConvertTo-Json -Depth 100 | Set-Content "$wtpPath\LocalState\settings.json"
+    Write-Host "Windows Terminal Preview settings.json updated."
+}
+
+if ($wtcPath) {
+    Write-Host "Windows Terminal Canary is installed."
+    if (Test-Path "$wtpPath\LocalState\settings.json") {
+        Write-Host "Windows Terminal Canary settings.json found."
+    } else {
+        Write-Host "Windows Terminal Canary settings.json not found, creating..."
+        $wtpExePath = Get-ChildItem "$env:ProgramFiles\WindowsApps\Microsoft.WindowsTerminalCanary_1*" -Directory | Select-Object -ExpandProperty FullName
         $mywtProcess = Get-Process -Name "WindowsTerminal"
         $mywtPid = $mywtProcess.Id
         Start-Process "$wtpExePath\wt.exe"
